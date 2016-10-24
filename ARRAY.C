274308860a3f1dee44e695fca4369c29a6e503cf
parth parmar <parthsparmar@github.com>
2016-10-24 18:03:40 +0530

    create array program

---
+++ ARRAY.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a [] = {0,1,2};
+	int b [] = {3,4,5};
+	int c [] = {6,7,8};
+	
+	clrscr();
+	printf("\n%d,%d,%d,%d,%d,%d,%d,%d,%d",a[0],b[0],c[0],a[1],b[1],c[1],a[2],b[2],c[2]);
+	getch();
+	
+}
